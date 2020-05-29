95eec699e2d7562d1acd01702a89fdce08ce5e55
Dênis Azevedo <denis.viegas.azevedo@gmail.com>
2020-05-28 23:09:39 -0300

    Primeiro Commit
    Teste Kenta

---
+++ Questao1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Questao1
+{
+    class Program
+    {
+        class Candidato
+        {
+            public string nome;
+            public int qtdVotos = 1;
+        }
+
+        private void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe a quantidade de votos:");
+                List<Candidato> votos = new List<Candidato>();
+                Candidato voto;
+                int qtdVotos = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i < qtdVotos + 1; i++)
+                {
+                    Console.WriteLine($"Informe o nome do candidato para o voto nº{i}:");
+                    string nomeCandidato = Console.ReadLine().ToUpper();
+                    voto = new Candidato { nome = nomeCandidato };
+                    if (!votos.Exists(v => v.nome == voto.nome))
+                        votos.Add(voto);
+                    else
+                        votos.Where(v => v.nome.ToUpper() == voto.nome.ToUpper()).FirstOrDefault().qtdVotos++;
+                }
+                Console.WriteLine($"O candidato mais votado é o(a): {vencedor(votos)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+
+        }
+
+        private string vencedor(List<Candidato> votos)
+        {
+            return votos.Where(v => v.qtdVotos == votos.Max(c => c.qtdVotos))
+                        .OrderBy(v => v.nome)
+                        .FirstOrDefault().nome
+                        .ToString();
+        }
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"28-05-2020 06:09:59 - {GetTurn(new DateTime(2020, 05, 28, 06, 09, 59))}");
+                Console.WriteLine($"28-05-2020 06:10:00 - {GetTurn(new DateTime(2020, 05, 28, 06, 10, 00))}");
+                Console.WriteLine($"28-05-2020 16:49:59 - {GetTurn(new DateTime(2020, 05, 28, 16, 49, 59))}");
+                Console.WriteLine($"28-05-2020 16:50:00 - {GetTurn(new DateTime(2020, 05, 28, 16, 50, 00))}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+
+        enum Turno { Diurno, Noturno }
+
+        static Turno GetTurn(DateTime dataHoraInformada)
+        {
+            TimeSpan horaInformada = new TimeSpan(dataHoraInformada.Hour, dataHoraInformada.Minute, dataHoraInformada.Second);
+
+            if (horaInformada < new TimeSpan(06, 10, 00) || horaInformada > new TimeSpan(16, 49, 59))
+                return Turno.Noturno;
+
+            return Turno.Diurno;
+        }
+    }
+}
